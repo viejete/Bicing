2e484b4cfa39975532c087ae1aad4d40730e46a5
viejete <53639858v@iespoblenou.org>
2017-02-03 17:52:40 +0100

    Permissions at runtime working

--- app/src/main/java/com/example/a53639858v/bicing/MapFragment.java
+++ app/src/main/java/com/example/a53639858v/bicing/MapFragment.java
@@ -1,7 +1,13 @@
 package com.example.a53639858v.bicing;
 
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +18,15 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 
+import java.util.HashMap;
+
 
 public class MapFragment extends Fragment {
 
     View view;
     MapView map;
     GeoPoint startPoint;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     public MapFragment() {
     }
@@ -26,6 +35,37 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        int permissioCheck = ContextCompat.checkSelfPermission(getContext() , Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        Log.i("permission" , "" + permissioCheck);
+
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(getContext() , Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION , Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
 
         map = (MapView) view.findViewById(R.id.map);
 
@@ -36,6 +76,7 @@ public class MapFragment extends Fragment {
         mapMarker();
 
         map.invalidate();
+
 
         return view;
     }
@@ -60,5 +101,52 @@ public class MapFragment extends Fragment {
 
         //startMarker.setIcon(getResources().    }
         startMarker.setTitle("Start point");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS: {
+
+                HashMap<String,Integer> perms = new HashMap<>();
+
+                perms.put(Manifest.permission.ACCESS_COARSE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.READ_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+
+                Log.i("permissions" , "hasta aqui bien?");
+                // If request is cancelled, the result arrays are empty.
+                if (perms.get(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    Snackbar.make(view , "holita" , Snackbar.LENGTH_LONG).show();
+
+                    Log.i("permissions" , "Que pollarda....");
+
+
+
+                } else {
+
+
+                    Snackbar.make(view , "holitamalo" , Snackbar.LENGTH_LONG).show();
+
+
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
